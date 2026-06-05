7c9561e5ba5eb0da09431d81b1ab92d248fe5791
KevinVivanco_02 <131885520+KevinParker02@users.noreply.github.com>
2026-06-05 16:58:37 -0400

    Fix reconocer patentes

--- README.md
+++ README.md
@@ -5,4 +5,10 @@ Para ejecutar esta basura debes
 1. Crear un entorno virtual (python -m venv venv)
 2. Activa el entorno virtual (venv\Scripts\activate)
 3. Instalas los requerimiento (pip install -r requirements.txt)
+3.1 Instalar PyTorch según la GPU disponible:
+    a.NVIDIA CUDA:
+        pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
+    b.CPU:
+        pip install torch torchvision
+
 4. Corres el programa con (python app.py)
--- detector.py
+++ detector.py
@@ -1,10 +1,21 @@
 import cv2
 import easyocr
 import re
+import torch
 
-reader = easyocr.Reader(['en'])
+reader = None
 
 def detect_plate(frame):
+
+    global reader
+
+    if reader is None:
+        print("Cargando EasyOCR...")
+        reader = easyocr.Reader(
+            ['en'],
+            gpu=torch.cuda.is_available()
+        )
+        print("EasyOCR cargado")
 
     h, w = frame.shape[:2]
 
@@ -30,12 +41,21 @@ def detect_plate(frame):
 
         plate = results[0][1].upper()
 
-        # Eliminar cualquier carácter que no sea letra o número
         plate = re.sub(
             r'[^A-Z0-9]',
             '',
             plate
         )
+
+        if len(plate) not in (5, 6):
+            return None
+
+        # Debe contener al menos una letra y un número
+        if not re.search(r'[A-Z]', plate):
+            return None
+
+        if not re.search(r'[0-9]', plate):
+            return None
 
         return plate
 
